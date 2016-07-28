9f6fe496023a113ed9e043e993b25d5ec7764009
Julien Levesy <jlevesy@gmail.com>
2016-07-28 14:52:09 +0200

    introduce the setup method

--- lib/rbutils/thrift/middleware.rb
+++ lib/rbutils/thrift/middleware.rb
@@ -1,3 +1,24 @@
 require 'rbutils/thrift/middleware/error_catcher'
 require 'rbutils/thrift/middleware/request_logger'
 require 'rbutils/thrift/middleware/timeout'
+
+module RbUtils
+  module Thrift
+    module Middleware
+      class << self
+        def setup(handler, logger, error_logger, timeout)
+          ErrorCatcher.new(
+            Timeout.new(
+              RequestLogger.new(
+                handler,
+                logger
+              ),
+              timeout
+            ),
+            error_logger
+          )
+        end
+      end
+    end
+  end
+end
