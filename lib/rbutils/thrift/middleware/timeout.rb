2c6275420c949a9d0c5c996b87aa8d0861e6de07
Julien Levesy <jlevesy@gmail.com>
2016-07-28 11:07:08 +0200

    add timeout middleware

---
+++ lib/rbutils/thrift/middleware/timeout.rb
@@ -0,0 +1,21 @@
+module RbUtils
+  module Thrift
+    module Middleware
+      class Timeout
+        def initialize(app, duration)
+          @app = app
+          @duration = duration
+        end
+
+        def method_missing(method, *args, &block)
+          ::Timeout.timeout(@duration) { @app.send(method, *args, &block) }
+        rescue ::Timeout::Error
+          raise ::Thrift::ApplicationException.new(
+            ::Thrift::ApplicationException::INTERNAL_ERROR,
+            'Timeout reached'
+          )
+        end
+      end
+    end
+  end
+end
