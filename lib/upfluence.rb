33f97f5e10aca6fbb6a7305fe43253a6b1c5bb9f
tgallice <tmgallice@gmail.com>
2016-11-22 11:29:55 +0100

    Add Active model serializers polyfill

--- lib/upfluence.rb
+++ lib/upfluence.rb
@@ -1,2 +1,3 @@
 require 'upfluence/utils'
 require 'upfluence/endpoint/api_endpoint'
+require 'upfluence/polyfill/active_model_serializers' unless ActiveModel::Serializer.respond_to?('serializer_for')
