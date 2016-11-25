88d15f385b8e62078f23e610a41ad1928d6e8b46
Thomas Gallice <tgallice@users.noreply.github.com>
2016-11-25 14:30:48 +0100

    Add some useful methods (#3)
    * Add json_params
    
    * Extract BadRequestException
    
    * Add the render_errors
    
    * Add active model serializers
    
    * Fix missing deps
    
    * Redefine helpers
    
    * Handle BadRequest by sinatra
    
    * Handle activeModel serializable
    
    * Add Active model serializers polyfill
    
    * Fix missing symbole
    
    * Remove render_error
    
    * Remove polyfill

--- lib/upfluence/endpoint/api_endpoint.rb
+++ lib/upfluence/endpoint/api_endpoint.rb
@@ -1,8 +1,9 @@
 module Upfluence
   module Endpoint
-    class ApiEndpoint < Sinatra::Base
-      class BadRequest < StandardError; end
 
+    class BadRequest < StandardError; end
+
+    class ApiEndpoint < Sinatra::Base
       disable :show_exceptions
 
       configure :development do
@@ -31,13 +32,28 @@ module Upfluence
                        ActiveModel::ArraySerializer.new(
                            resource, *args
                        ).to_json
-                     else
+                     elsif resource.respond_to?(:serialize)
                        resource.serialize.to_json
+                     else
+                       ActiveModel::Serializer.serializer_for(resource).new(resource).to_json
                      end
           end
           halt [status, result] || 404
         end
+
+        def json_params
+          begin
+            ActiveSupport::HashWithIndifferentAccess.new(JSON.parse(request.body.read))
+          rescue
+            halt 400, { message: 'Invalid JSON' }.to_json
+          end
+        end
       end
+    end
+
+    Sinatra::Base.error BadRequest do
+      status 400
+      { error: 'Bad request' }.to_json
     end
   end
 end
