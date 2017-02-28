6d0fe6e91eadad8e98490df74162d42236a1fd11
Philippe Ndiaye <phndiaye@gmail.com>
2017-02-28 20:52:29 +0100

    fix imhotep comments

--- lib/upfluence/base/exceptions/validation_error.rb
+++ lib/upfluence/base/exceptions/validation_error.rb
@@ -12,7 +12,7 @@ module Base
           model.errors.details.map do |error_field, errors|
             errors.map do |error|
               validation_errors << Base::Exceptions::Validation.new(
-                domain: self.domain,
+                domain: domain,
                 model: model.model_name.singular,
                 field: error_field.to_s,
                 error: error[:error].to_s
