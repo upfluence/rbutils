f3e6dc8f1da996b4041a57fb2a51b95d869fded3
Philippe Ndiaye <phndiaye@gmail.com>
2017-02-28 21:33:38 +0100

    assign validation_errors var directly

--- lib/upfluence/base/exceptions/validation_error.rb
+++ lib/upfluence/base/exceptions/validation_error.rb
@@ -7,18 +7,16 @@ module Base
         attr_accessor :domain
 
         def from_model(model)
-          validation_errors = []
-
-          model.errors.details.map do |error_field, errors|
+          validation_errors = model.errors.details.map do |error_field, errors|
             errors.map do |error|
-              validation_errors << Base::Exceptions::Validation.new(
+              Base::Exceptions::Validation.new(
                 domain: domain,
                 model: model.model_name.singular,
                 field: error_field.to_s,
                 error: error[:error].to_s
               )
             end
-          end
+          end.flatten
 
           new(validations: validation_errors)
         end
