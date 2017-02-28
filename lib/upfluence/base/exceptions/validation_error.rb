bdd0c0d00d3f43f78005a456cfc870d837ae4e87
Philippe Ndiaye <phndiaye@gmail.com>
2017-02-28 20:50:25 +0100

    fix identation issue

--- lib/upfluence/base/exceptions/validation_error.rb
+++ lib/upfluence/base/exceptions/validation_error.rb
@@ -7,20 +7,20 @@ module Base
         attr_accessor :domain
 
         def from_model(model)
-         validation_errors = []
+          validation_errors = []
 
-         model.errors.details.map do |error_field, errors|
-           errors.map do |error|
-             validation_errors << Base::Exceptions::Validation.new(
-               domain: self.domain,
-               model: model.model_name.singular,
-               field: error_field.to_s,
-               error: error[:error].to_s
-             )
-           end
-         end
+          model.errors.details.map do |error_field, errors|
+            errors.map do |error|
+              validation_errors << Base::Exceptions::Validation.new(
+                domain: self.domain,
+                model: model.model_name.singular,
+                field: error_field.to_s,
+                error: error[:error].to_s
+              )
+            end
+          end
 
-         new(validations: validation_errors)
+          new(validations: validation_errors)
         end
       end
     end
